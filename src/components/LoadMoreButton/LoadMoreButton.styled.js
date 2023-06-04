2bdfb7d541826f21889f525255b0f008d5cbe2b0
Rostyk Podolets <ros.podolets@gmail.com>
2023-06-04 14:18:02 +0300

    Load more button is created

--- src/components/LoadMoreButton/LoadMoreButton.styled.js
+++ src/components/LoadMoreButton/LoadMoreButton.styled.js
@@ -10,6 +10,8 @@ export const Button = styled.button`
   border-radius: 10px;
   border: none;
   background-color: #5cd3a8;
+  box-shadow: -2.5777px 6.87386px 20.6216px rgba(0, 0, 0, 0.23);
+
   cursor: pointer;
 
   font-family: 'Montserrat';
@@ -19,4 +21,14 @@ export const Button = styled.button`
   line-height: 1.222;
   text-transform: uppercase;
   color: #373737;
+
+  &:hover,
+  &:focus {
+    background-image: linear-gradient(
+      114.99deg,
+      #32ac80 -0.99%,
+      #5cd3a8 54.28%,
+      #32ac80 78.99%
+    );
+  }
 `;
